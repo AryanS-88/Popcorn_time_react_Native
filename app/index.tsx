0d5280b2aeba1d02091ab3b0344730c7d1ac9efc
AryanS-88 <ssingharyan70@gmail.com>
2025-05-25 13:24:31 +0530

    eslint and prettier

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 
 export default function index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-4xl font-bold">Namastae</Text>
+      <Text className="text-4xl font-bold text-red-500">Namastae</Text>
+      <StatusBar style="auto" />
     </View>
   )
 }
