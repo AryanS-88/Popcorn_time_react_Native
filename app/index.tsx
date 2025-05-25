3d063ecb5dd615cdc317dd03db4c0b09247fe7c5
AryanS-88 <ssingharyan70@gmail.com>
2025-05-25 09:26:18 +0530

    style setup and required changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,10 @@
+import React from 'react'
 import { Text, View } from 'react-native'
 
-export default function Index() {
+export default function index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Namastae</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl font-bold">Namastae</Text>
     </View>
   )
 }
